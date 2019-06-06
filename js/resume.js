a3d519f45027b40c2db5b79cde8e39a8cf64d692
siriphonnot <siriphonnot@gmail.com>
2019-06-06 18:47:55 +0700

    Add function zoom image

--- js/resume.js
+++ js/resume.js
@@ -33,4 +33,27 @@
     $(this).next('.view-message').css('display', 'block');
   });
 
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("zoomImg");
+  var captionText = document.getElementsByClassName("zoomImgCaption")[0];
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  $('.item-profile img').on('click', function(){
+    if($(this).hasClass('nolink')){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+    }
+  });
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+
 })(jQuery); // End of use strict
